0018f45161b8c1f1b0e12e60a8202e56c606af3e
krishna <kv.250307@gmail.com>
2026-05-29 12:19:08 +0530

    File calling and collecting text

--- linux_command_freq.c
+++ linux_command_freq.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+void cond(FILE *f){
+    if(f==NULL){
+        printf("\nError in finding the file...\n");
+        exit(0);
+    }
+    else{
+        printf("\nFile found successfully\n");
+    }
+}
+
+void inp(FILE *f,FILE *f_w){
+    // int index;
+    char ch=fgetc(f);;
+    while(ch!=EOF){
+        fputc(ch,f_w);
+        ch=fgetc(f);
+        // printf("%c",ch);
+    }
+}
+
+
+
 int main(){
     char usr[50];
     printf("Please enter you username...:");
     fgets(usr,sizeof(usr),stdin);
     usr[strcspn(usr,"\n")]=0;
     char loc[100];
-    char us[10]="/home";
-    char us1[20]= ".bash_history";
+    char us[]="/home";
+    char us1[]= ".bash_history";
     snprintf(loc,sizeof(loc),"%s/%s/%s",us,usr,us1);
     printf("%s",loc);
     FILE *f =  fopen(loc,"r");
-
+    FILE *f_w = fopen("freq.txt","w");
+    cond(f);
+    inp(f,f_w);
     fclose(f);
+    fclose(f_w);
 }
