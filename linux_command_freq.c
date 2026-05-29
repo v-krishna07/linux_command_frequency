817d13e42e15267608cb61616830e7a4d40abd31
krishna <kv.250307@gmail.com>
2026-05-29 11:56:50 +0530

    Initial commit

--- linux_command_freq.c
+++ linux_command_freq.c
@@ -3,10 +3,8 @@
 int main(){
     char usr[50];
     printf("Please enter you username...:");
-    // scanf(" %s",usr);
     fgets(usr,sizeof(usr),stdin);
     usr[strcspn(usr,"\n")]=0;
-    // char loc[100] = ("/home/%s/.bash_history",usr);
     char loc[100];
     char us[10]="/home";
     char us1[20]= ".bash_history";
